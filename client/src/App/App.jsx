964aa2b35269f180219ea41f8ae42f03bb241edf
todovica <ana.todovic@gmail.com>
2019-10-24 20:57:30 +0200

    adding scss to project

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import '../index.css';
+import '../index.scss';
 import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
